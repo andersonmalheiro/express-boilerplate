75c26bfd66b0264f525886c1fa429841a335f3e3
Gabriel Lima <gabriel.lima@aluno.fapce.edu.br>
2019-10-03 10:22:10 -0300

    feat(Models): Creating user model

---
+++ src/models/user/schema.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Schema de usuário
+const schema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: [true],
+    },
+    avatar: {
+      type: String,
+      required: [false],
+    },
+    password: {
+      type: String,
+      required: [true],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = { schema };
